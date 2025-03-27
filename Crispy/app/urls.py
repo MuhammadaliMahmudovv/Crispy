9f2a5f71fe1c3e42e913e3d432d5d34fe1010ac0
MuhammadaliMahmudovv <muhammadalimahmudovv23@gmail.com>
2025-03-27 13:56:53 +0500

    8

--- Crispy/app/urls.py
+++ Crispy/app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('menu/', views.menu, name='menu'),
+    path('news/', views.menu, name='news'),
+    path('contact/', views.menu, name='contact'),  
 ]
--- Crispy/app/views.py
+++ Crispy/app/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app/index.html')
+
+def about(request):
+    return render(request, 'app/about.html')
+
+def menu(request):
+    return render(request, 'app/menu.html')
+
+def news(request):
+    return render(request, 'app/news.html')
+
+def contact(request):
+    return render(request, 'app/contact.html')
