07c03305267688bb9ddeec14283e1ea605582e6e
MuhammadaliMahmudovv <muhammadalimahmudovv23@gmail.com>
2025-03-27 14:31:33 +0500

    9

--- Crispy/app/urls.py
+++ Crispy/app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('menu/', views.menu, name='menu'),
-    path('news/', views.menu, name='news'),
-    path('contact/', views.menu, name='contact'),  
+    path('news/', views.news, name='news'),
+    path('contact/', views.contact, name='contact'),  
 ]
